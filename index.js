0d9ed62262a0adde8a926f850b92342fb1b6f32d
nichmatt <nicholas.official17@gmail.com>
2024-03-29 21:39:53 +0700

    fix: 🐛 index
    index

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 "use strict";
 
-require("dotenv").config();
+const PORT = 3001;
 
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const router = require("./routers");
-const { errorHandler } = require("./middlewares");
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+const { app } = require("./app");
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
 });
-
-pool.connect((err) => {
-  if (err) throw er;
-  console.log("Connect to PostgreSql succesfully");
-});
-
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(router);
-app.use(errorHandler);
-
-module.exports = { app };
