700df0f11e5139f0abab1b86e38531f30c725aa7
nichmatt <nicholas.official17@gmail.com>
2024-03-30 08:05:23 +0700

    fix: 🐛 structure
    structure

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-"use strict";
+require("dotenv").config();
 
+const express = require("express");
 const PORT = 3001;
+const cors = require("cors");
+const app = express();
+const router = require("./routers");
+const { errorHandler } = require("./middlewares");
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
 
-const { app } = require("./app");
-// test deploy
+pool.connect((err) => {
+  if (err) throw er;
+  console.log("Connect to PostgreSql succesfully");
+});
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(router);
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
